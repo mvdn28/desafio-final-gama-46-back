336af1442cf950c076a9078aabcf61598b4d6b3e
mvdn28 <vieira.mauricio94@gmail.com>
2023-03-01 17:12:48 -0300

    admin migration

---
+++ migrations/20230301200711-add-admin-user.js
@@ -0,0 +1,22 @@
+const { Db, Admin } = require("mongodb");
+require('dotenv').config()
+const bcrypt = require('bcrypt');
+
+const hashPass = async() =>{
+  return await bcrypt.hash(process.env.ADMPASSWORD,10)
+}
+
+module.exports = {
+  async up(db) {
+    await db.collection('User').insertOne({
+      name: process.env.ADMNAME,
+      email: process.env.ADMEMAIL,
+      password: await hashPass(),
+      isAdmin: true
+    });
+  },
+
+  async down(db) {
+    await db.collection('User').deleteOne({ email: process.env.ADMEMAIL });
+  }
+};
