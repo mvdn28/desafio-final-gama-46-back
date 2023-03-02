bcb3ccf06b548f0738f0038e2ada1e5c9e47776d
mvdn28 <vieira.mauricio94@gmail.com>
2023-03-02 16:14:48 -0300

    config MONGODB URL

--- src/database/config.ts
+++ src/database/config.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default  {
-    uri:process.env.DBURI as string,
+    uri:process.env.MONGO_URL as string,
     port: process.env.DBPORT as string,
     host: process.env.DBHOST as string
 }
