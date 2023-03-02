3bfe4c88c6cda62e7f11bbc9a5775eaed0d150fb
mvdn28 <vieira.mauricio94@gmail.com>
2023-03-02 08:32:57 -0300

    middleware CheckAdmin

---
+++ src/middlewares/CheckAdmin.ts
@@ -0,0 +1,8 @@
+import { NextFunction, Request, Response } from "express";
+
+export const CheckAdmin =  (req:any,res: Response, next: NextFunction) => {
+    if (req.userRole =!"admin")
+        return res.status(401).send({ message: "Ação proíbida, usuário não é admin" });
+
+    next();
+}
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,14 +1,15 @@
 import {Router} from "express"
 import { UserController } from "../controller/UserController";
+import { CheckAdmin } from "../middlewares/CheckAdmin";
 import { CheckAuth } from "../middlewares/CheckAuth";
 
 const router = Router()
 
 router.get("/",[], UserController.findAll);
 router.get("/:id",[], UserController.findOne);
-router.post("/",[CheckAuth],UserController.create);
-router.put("/",[CheckAuth], UserController.update);
-router.delete("/",[CheckAuth], UserController.findAndDelete);
+router.post("/",[CheckAuth, CheckAdmin],UserController.create);
+router.put("/",[CheckAuth, CheckAdmin], UserController.update);
+router.delete("/",[CheckAuth, CheckAdmin], UserController.findAndDelete);
 
 
 export default router
