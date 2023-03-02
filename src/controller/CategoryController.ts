28ee47e96a27e26cb922654fa764ed374ebe31c7
mvdn28 <vieira.mauricio94@gmail.com>
2023-03-02 08:04:15 -0300

    padronização de mensagens

--- src/controller/CategoryController.ts
+++ src/controller/CategoryController.ts
@@ -15,8 +15,8 @@ export class CategoryController{
         const id = req.params.id
 
         return await Category.findById(id)
-            .then((category)=>{category ? res.status(200).json(category) : res.status(404).json({message:"Category not found"})})
-            .catch((error) => res.status(500).json({error}))
+            .then((category)=>{category ? res.status(200).json({category}) : res.status(404).json({message:"Categoria não encontrada"})})
+            .catch((error) => res.status(500).json({error,message:'Ocorreu um erro ao buscar os detalhes da categoria.'}))
     }
 
     static create = async (req: Request, res: Response): Promise<Response> => {
@@ -28,7 +28,7 @@ export class CategoryController{
 
         return await newCategory.save()
             .then((category) => res.status(201).json({category}))
-            .catch((error) => res.status(500).json({error}))
+            .catch((error) => res.status(500).json({error,message:'Ocorreu um erro ao cadastrar a categoria.'}))
     }
 
     static update = async (req: Request, res: Response): Promise<Response> => {
@@ -37,15 +37,15 @@ export class CategoryController{
         const update = await Category.findOneAndReplace({_id:id},{ name })
 
         return await Category.findById(id)
-            .then((updatedCategory)=>update ? res.status(200).json({updatedCategory}) : res.status(404).json({message:"Category not found"}))
-            .catch((error)=>res.status(500).json({error}))
+            .then((updatedCategory)=>update ? res.status(200).json({updatedCategory}) : res.status(404).json({message:"Categoria não encontrada"}))
+            .catch((error)=>res.status(500).json({error, message: 'Ocorreu um erro ao atualizar a categoria'}))
     }
 
     static findAndDelete = async (req: Request, res: Response): Promise<void | Response> => {
         const id = req.params.id
 
         return await Category.findByIdAndDelete(id)
-            .then((category)=> category ? res.status(201).json({category,message:"deleted"}) : res.status(404).json({category,message:"Category not found"}))
-            .catch((error)=> res.status(500).json({error}))
+            .then((category)=> category ? res.status(201).json({category,message: 'Categoria excluída com sucesso.'}) : res.status(404).json({category,message:"Categoria não encontrada"}))
+            .catch((error)=> res.status(500).json({error, message: 'Ocorreu um erro ao deletar a categoria'}))
     }
 }
