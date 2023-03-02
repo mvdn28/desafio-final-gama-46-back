3c300e89814b63bbe137b11d31cb83282fe19ab4
mvdn28 <vieira.mauricio94@gmail.com>
2023-03-02 08:38:31 -0300

    padronização entity

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import { User } from '../entities/User';
+import User from '../entities/User';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv';
