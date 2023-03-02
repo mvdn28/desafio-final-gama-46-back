aaf87c17d9e252af87385f7eb48b998a6ca25132
mvdn28 <vieira.mauricio94@gmail.com>
2023-03-02 08:19:11 -0300

    padronização de mensagens

--- src/controller/OrderController.ts
+++ src/controller/OrderController.ts
@@ -7,20 +7,24 @@ export class OrderController{
     static findAll =async (req:Request,res:Response) => {
         return await Order.find()
             .then((orders)=> res.status(200).json({orders}))
-            .catch((error)=>res.status(500).json({error}))
+            .catch((error)=>res.status(500).json({error, message:'Ocorreu um erro ao listar os pedidos.'}))
     }
 
     static findOne = async (req: Request, res: Response): Promise<void | Response> => {
         const id = req.params.id
 
         return await Order.findById(id)
-            .then((order)=>{order ? res.status(200).json({order}) : res.status(404).json({message:"Order not found"})})
-            .catch((error) => res.status(500).json({error}))
+            .then((order)=>{order ? res.status(200).json({order}) : res.status(404).json({message:"Pedido não encontrado."})})
+            .catch((error) => res.status(500).json({error,message:"Ocorreu um erro ao listar o pedido"}))
     }
 
-    static create = async (req: Request, res: Response): Promise<Response> => {
-        const { products, user } = req.body
-        const total = products.reduce((a:IProduct,b:IProduct)=>a.price + b.price)
+    static create = async (req: any, res: Response): Promise<Response> => {
+        const { products } = req.body
+        const user = req.userId
+        let total = 0;
+        products.forEach((element: { price: number })=>{
+            total+=element.price
+        })
         const newOrder = new Order({
             _id:new Types.ObjectId(),
             products,
@@ -29,26 +33,30 @@ export class OrderController{
         }as unknown as Document<IOrder>)
 
         return await newOrder.save()
-            .then((Order) => res.status(201).json({Order}))
-            .catch((error) => res.status(500).json({error}))
+            .then((order) => res.status(201).json({order}))
+            .catch((error) => res.status(500).json({error,message:"Ocorreu um erro ao criar o pedido"}))
     }
 
-    static update = async (req: Request, res: Response): Promise<Response> => {
+    static update = async (req: any, res: Response): Promise<Response> => {
         const id = req.params.id
-        const { products, user } = req.body
-        const total = products.reduce((a:IProduct,b:IProduct)=>a.price + b.price)
+        const { products} = req.body
+        const user = req.userId
+        let total = 0;
+        products.forEach((element: { price: number })=>{
+            total+=element.price
+        })
         await Order.findOneAndReplace({_id:id},{products,total,user})
         
         return await Order.findById(id)
-            .then((updatedOrder)=>updatedOrder ? res.status(200).json({updatedOrder}) : res.status(404).json({message:"Order not found"}))
-            .catch((error)=>res.status(500).json({error}))
+            .then((updatedOrder)=>updatedOrder ? res.status(200).json({updatedOrder}) : res.status(404).json({message:"Pedido não encontrado"}))
+            .catch((error)=>res.status(500).json({error,message:"Ocorreu um erro ao atualizar o pedido"}))
     }
 
     static findAndDelete = async (req: Request, res: Response): Promise<void | Response> => {
         const id = req.params.id
 
         return await  Order.findByIdAndDelete(id)
-            .then((order)=> order ? res.status(201).json({order,message:"deleted"}) : res.status(404).json({order,message:"Order not found"}))
-            .catch((error)=> res.status(500).json({error}))
+            .then((order)=> order ? res.status(201).json({order,message:"Pedido excluído com sucesso"}) : res.status(404).json({order,message:"Pedido não encontrado"}))
+            .catch((error)=> res.status(500).json({error,message:"Ocorreu um erro ao deletar o pedido"}))
     }
 }
