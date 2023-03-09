0546a390b38d4363d6be36c2f048fc6775a6da20
mvdn28 <vieira.mauricio94@gmail.com>
2023-03-09 14:53:04 -0300

    user check update

--- src/middlewares/CheckUserAdmin.ts
+++ src/middlewares/CheckUserAdmin.ts
@@ -6,21 +6,30 @@ dotenv.config()
 export const CheckUserAdmin = (req:any,res: Response, next: NextFunction) => {
     if (req.body.role != 'admin'){
         next()
-    }
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).json({ message: 'Token de autenticação não fornecido.' });
-    }
-    const decodedToken = jwt.verify(authHeader, process.env.JWT as string) as any;
-    if (!decodedToken){
-        return res.status(401).json({ message: 'Token de autenticação inválido.' });
-    }
-    req.userId = decodedToken.userId;
-    req.userRole = decodedToken.role;
-    if(req.userRole === 'admin'){
-        next();
     }else{
-        return res.status(401).json({ message: 'Token de autenticação inválido.' });
+        try {
+            const authHeader = req.headers.authorization;
+            if (!authHeader) {
+                res.status(401).json({ message: 'Token de autenticação não fornecido.' });
+            }
+            const decodedToken = jwt.verify(authHeader, process.env.JWT as string) as any;
+            if (!decodedToken){
+                res.status(401).json({ message: 'Token de autenticação inválido.' });
+            }
+            req.userId = decodedToken.userId;
+            req.userRole = decodedToken.role;
+            if(req.userRole === 'admin'){
+                next();
+            }else{
+                res.status(401).json({ message: 'Token de autenticação inválido.' });
+            }
+            
+        } catch (error) {
+            res.status(500).json({message:'Token de autenticação inválido.'})
+        }
+        
+        
     }
+    
     
 }
